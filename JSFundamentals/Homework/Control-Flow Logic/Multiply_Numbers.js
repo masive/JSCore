db6499671afc22f60b46e78932dee4b4feb021f4
Pavel Anchev <masive@abv.bg>
2016-09-20 17:58:31 +0300

    First exercise Lab 2

---
+++ JSFundamentals/Homework/Control-Flow Logic/Multiply_Numbers.js
@@ -0,0 +1,7 @@
+function MultiplyNumbers(args){
+    let [a, b] = [Number(args[0]), Number(args[1])];
+    return a * b;   
+}
+
+MultiplyNumbers(['2', '3']);
+MultiplyNumbers(['23632.36', '-12.3249']);

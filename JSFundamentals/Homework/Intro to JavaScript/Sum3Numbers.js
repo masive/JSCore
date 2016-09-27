43fe0c0d4d7fe9d6d3890c197296a96090e5ded8
Pavel Anchev <masive@abv.bg>
2016-09-27 16:48:27 +0300

    Last changes

--- JSFundamentals/Homework/Intro to JavaScript/Sum3Numbers.js
+++
@@ -1,9 +0,0 @@
-function sumThreeNumbers (args) {
-    let numOne = Number(args[0]);
-    let numTwo = Number(args[1]);
-    let numThree = Number(args[2]);
-    let sum = numOne + numTwo + numThree;
-    console.log(sum); 
-}
-
-sumThreeNumbers(['1', '2', '3']);

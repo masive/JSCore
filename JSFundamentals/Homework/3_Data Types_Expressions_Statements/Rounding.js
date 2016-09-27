752f8810e76f962d33a818464ba62abe2dfdd182
Pavel Anchev <masive@abv.bg>
2016-09-27 17:40:28 +0300

    Exercise 7 completed

---
+++ JSFundamentals/Homework/3_Data Types_Expressions_Statements/Rounding.js
@@ -0,0 +1,20 @@
+function rounding (args) {
+  let str = args[0]
+  let n = Number(args[1])
+  let firstPart = str.substr(0, str.indexOf('.'))
+  let secondPart = str.substr(str.indexOf('.'))
+  let num = 0
+  if (secondPart.length > 15) {
+    secondPart = secondPart.slice(0, 15 - secondPart.length + 1)
+    num = Number(firstPart + secondPart).toFixed(n)
+  } else if (secondPart.length - 1 < n) {
+    num = Number(firstPart + secondPart)
+  } else {
+    num = Number(firstPart + secondPart).toFixed(n)
+  }
+
+  console.log(num)
+}
+
+rounding(['3.141872264546464615555', '2'])
+rounding(['10.59', '2'])

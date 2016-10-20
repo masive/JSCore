eb183ae4698f60dbd42536a6ebddcdc8247290ab
masive <masive@abv.bg>
2016-10-20 22:41:12 +0300

    02. Search in List

--- JSAdvanced/Homework/jQuery Lab/search.js
+++ JSAdvanced/Homework/jQuery Lab/search.js
@@ -1,3 +1,14 @@
 function search() {
-
+    let searchText = $('#searchText').val();
+    let count = 0;
+    $('#towns li').each(function () {
+        if ($(this).text().indexOf(searchText) != -1) {
+            $(this).css('font-weight', 'bold');
+            count++;
+        } else {
+            $(this).css('font-weight', '');
+        }
+        $('#result').text(count + " matches found.");
+        $('#searchText').val('');
+    })
 }
